7f642be37458a09cf69e36e50b6100a695937b59
Raphael Boitte <raphael.boitte@gmail.com>
2022-05-24 19:56:16 +0200

    Ajouté le - au feedback

--- PositivePositionFeedback.m
+++ PositivePositionFeedback.m
@@ -2,9 +2,6 @@ close all;
 
 sys = open("sys.mat").sys;
 
-in_ID=2;
-out_ID=2;
-perf_ID=1;
 %% compliance of the whole syst
 close all;
 bodemag(sys)
@@ -19,15 +16,15 @@ sisotool(sys(2,2));
 % Cppf2 = 2nd order ppf
 %% SISO - Closing the loop with C1ppf 
 
-syscontrolled=feedback(sys,Cppf3,2,2);
+syscontrolled=feedback(sys,-Cppf1,2,2);
 bodemag(sys(1,1))
 hold on
 bodemag(syscontrolled(1,1))
 legend("openloop","closedloop1")
 %% Compare siso of cppf1 and 2
 close all;
-syscontrolled1=feedback(sys,Cppf1,2,2);
-syscontrolled2=feedback(sys,Cppf2,2,2); 
+syscontrolled1=feedback(sys,-Cppf1,2,2);
+syscontrolled2=feedback(sys,-Cppf2,2,2); 
 bodemag(sys(1,1))
 hold on
 bodemag(syscontrolled1(1,1))
