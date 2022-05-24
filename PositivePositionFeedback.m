cc5a4b1fc37a675e12a45f7a9a47ab3bdca6dfa2
Raphael Boitte <raphael.boitte@gmail.com>
2022-05-24 19:01:55 +0200

    Added matrix

--- PositivePositionFeedback.m
+++ PositivePositionFeedback.m
@@ -15,3 +15,24 @@ bodemag(sys(1,1))
 
 %% SISO First order PPF on first patch
 sisotool(sys(2,2));
+% Cppf1 = first order ppf
+% Cppf2 = 2nd order ppf
+%% SISO - Closing the loop with C1ppf 
+
+syscontrolled=feedback(sys,Cppf3,2,2);
+bodemag(sys(1,1))
+hold on
+bodemag(syscontrolled(1,1))
+legend("openloop","closedloop1")
+%% Compare siso of cppf1 and 2
+close all;
+syscontrolled1=feedback(sys,Cppf1,2,2);
+syscontrolled2=feedback(sys,Cppf2,2,2); 
+bodemag(sys(1,1))
+hold on
+bodemag(syscontrolled1(1,1))
+hold on
+bodemag(syscontrolled2(1,1))
+legend("openloop","closedloop1","closedloop2","3")
+
+
