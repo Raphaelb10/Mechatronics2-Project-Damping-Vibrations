d6921ce6e252086b85baf26797473d616fb7ced5
Serplet <tigran@pletser.be>
2022-05-26 16:56:39 +0200

    Added NDF controller on fifth patch for second mode and decentralised mimo of both ndf controllers

--- NDF.m
+++ NDF.m
@@ -13,25 +13,67 @@ fifth_patch = [6,6];
 
 %% Compliance of the whole unchanged system
 
-% bodemag(sys)
+bodemag(sys)
 
 %% Perfomance index
-
+figure
 bodemag(sys(perf_index(1),perf_index(2)))
+
+%% Each piezo impact separatly
+figure
+bodemag(sys(first_patch(1),first_patch(2)))
+figure
+bodemag(sys(second_patch(1),second_patch(2)))
+figure
+bodemag(sys(third_patch(1),third_patch(2)))
+figure
+bodemag(sys(fourth_patch(1),fourth_patch(2)))
+figure
+bodemag(sys(fifth_patch(1),fifth_patch(2)))
+
+%% Each piezo impact comparaison
+
+figure
+bodemag(sys(first_patch(1),first_patch(2)))
+hold on
+bodemag(sys(second_patch(1),second_patch(2)))
+hold on
+bodemag(sys(third_patch(1),third_patch(2)))
+hold on
+bodemag(sys(fourth_patch(1),fourth_patch(2)))
+hold on
+bodemag(sys(fifth_patch(1),fifth_patch(2)))
+
+legend
 
 %% Sisotool first patch
 
 sisotool(sys(first_patch(1),first_patch(2)))
 
+%% Sisotool fifth patch
+
+sisotool(sys(fifth_patch(1),fifth_patch(2)))
+
 %% Damping evaluation SISO
 
-syscontrolled=feedback(sys,Cndf1,first_patch(1),first_patch(2),1);
+syscontrolled1=feedback(sys,Cndf1,first_patch(1),first_patch(2),1);
+syscontrolled2 = feedback(sys,Cndf2,fifth_patch(1),fifth_patch(2));
 
 figure
 bodemag(sys(perf_index(1),perf_index(2)))
 hold on
-bodemag(syscontrolled(perf_index(1),perf_index(2)))
+bodemag(syscontrolled1(perf_index(1),perf_index(2)))
+hold on
+bodemag(syscontrolled2(perf_index(1),perf_index(2)))
+legend('no control','NDF first peak', 'NDF second peak')
+
+%% Damping evaluation MIMO
+
+syscontrolled1=feedback(sys,Cndf1,first_patch(1),first_patch(2),1);
+syscontrolled1 = feedback(syscontrolled1,Cndf2,fifth_patch(1),fifth_patch(2));
+
+figure
+bodemag(sys(perf_index(1),perf_index(2)))
+hold on
+bodemag(syscontrolled1(perf_index(1),perf_index(2)))
 legend('no control','NDF')
-
-
-
