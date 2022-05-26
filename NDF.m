6475f669f5305acb281fd96e81cd867a55b86f19
Serplet <tigran@pletser.be>
2022-05-26 12:45:06 +0200

    added pres pdf and changed NDT to NDF (its real name)

--- NDF.m
+++ NDF.m
@@ -5,6 +5,11 @@ close all;
 sys = open("sys.mat").sys;
 
 perf_index = [1,1];
+first_patch = [2,2];
+second_patch = [3,3];
+third_patch = [4,4];
+fourth_patch = [5,5];
+fifth_patch = [6,6];
 
 %% Compliance of the whole unchanged system
 
@@ -13,4 +18,8 @@ bodemag(sys)
 %% Perfomance index
 
 bodemag(sys(perf_index(1),perf_index(2)))
+
+%% Sisotool first patch
+
+sisotool(sys(first_patch(1),first_patch(2)))
 
