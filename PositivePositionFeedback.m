df565218e7db6e765646e35704afa9dac7eed55b
Raphael Boitte <raphael.boitte@gmail.com>
2022-06-01 17:22:05 +0200

    PPF graphs drone

--- PositivePositionFeedback.m
+++ PositivePositionFeedback.m
@@ -17,6 +17,8 @@ sisotool(sys(2,2));
 % Cppf3 = 1st order ppf with pole at 5th peak
 % Cppf4 = 2nd order ppf tune for 1st peak
 % Cppf5 = 1st order ppf with pole at 3rd peak
+% Cppf6 = Cppf5 with less gain
+% Cppf7 = Cppf3 with less gain, less improvement.
 %% SISO - Closing the loop with C1ppf 
 
 syscontrolled=feedback(sys,-Cppf1,2,2);
@@ -34,15 +36,21 @@ sisotool(sys(2,2))
 
 close all;
 syscontrolled1=feedback(sys,-Cppf1,2,2);
-% syscontrolled2=feedback(sys,-Cppf2,2,2); 
+syscontrolled2=feedback(sys,-Cppf2,2,2); 
 syscontrolled3=feedback(sys,-Cppf3,2,2); 
 syscontrolled5=feedback(sys,-Cppf5,2,2); 
+syscontrolled6=feedback(sys,-Cppf6,2,2); 
+syscontrolled7=feedback(sys,-Cppf7,2,2); 
+
 bodemag(sys(1,1))
 hold on
-bodemag(syscontrolled1(1,1))
-hold on
-bodemag(syscontrolled3(1,1))
-hold on
+% bodemag(syscontrolled(1,1))
+% hold on
 bodemag(syscontrolled5(1,1))
+hold on
+bodemag(syscontrolled6(1,1))
 % legend("openloop","closedloop1","closedloop2","closedloop3")
-legend("openloop","Cppf1","Cppf3","Cppf5")
+legend("openloop","Small gain margin","Bigger gain margin")
+grid on
+%%
+rlocus(syscontrolled6(1,1))
