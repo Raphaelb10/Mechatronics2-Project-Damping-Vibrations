c0333fe70e133f22a01117bddcd15ea72407741b
Serplet <tigran@pletser.be>
2022-06-12 14:48:16 +0200

    added soe nice graph RL and shit for Lead

--- Lead_Tigran.m
+++ Lead_Tigran.m
@@ -11,6 +11,9 @@ third_patch = [4,4];
 fourth_patch = [5,5];
 fifth_patch = [6,6];
 
+Linewidth = 2;
+FontSize = 15;
+
 %% Compliance of the whole unchanged system
 
 bodemag(sys)
@@ -35,16 +38,82 @@ bodemag(sys(fifth_patch(1),fifth_patch(2)))
 
 figure
 bodemag(sys(first_patch(1),first_patch(2)))
+Fh = gcf;                                                   % Handle To Current Figure
+Kids = Fh.Children;                                         % Children
+AxAll = findobj(Kids,'Type','Axes');                        % Handles To Axes
+Ax1 = AxAll(1);                                             % First Set Of Axes
+LinesAx1 = findobj(Ax1,'Type','Line');                      % Handle To Lines
+LinesAx1(2).LineWidth = Linewidth;                                  % Set ‘LineWidth’
+Ax2 = AxAll(2);                                             % Second Set Of Axes
+LinesAx2 = findobj(Ax2,'Type','Line');                      % Handle To Lines
+LinesAx2(2).LineWidth = Linewidth;                                  % Set ‘LineWidth’
+
 hold on
 bodemag(sys(second_patch(1),second_patch(2)))
+Fh = gcf;                                                   % Handle To Current Figure
+Kids = Fh.Children;                                         % Children
+AxAll = findobj(Kids,'Type','Axes');                        % Handles To Axes
+Ax1 = AxAll(1);                                             % First Set Of Axes
+LinesAx1 = findobj(Ax1,'Type','Line');                      % Handle To Lines
+LinesAx1(2).LineWidth = Linewidth;                                  % Set ‘LineWidth’
+Ax2 = AxAll(2);                                             % Second Set Of Axes
+LinesAx2 = findobj(Ax2,'Type','Line');                      % Handle To Lines
+LinesAx2(2).LineWidth = Linewidth;                                  % Set ‘LineWidth’
+
+
 hold on
 bodemag(sys(third_patch(1),third_patch(2)))
+Fh = gcf;                                                   % Handle To Current Figure
+Kids = Fh.Children;                                         % Children
+AxAll = findobj(Kids,'Type','Axes');                        % Handles To Axes
+Ax1 = AxAll(1);                                             % First Set Of Axes
+LinesAx1 = findobj(Ax1,'Type','Line');                      % Handle To Lines
+LinesAx1(2).LineWidth = Linewidth;                                  % Set ‘LineWidth’
+Ax2 = AxAll(2);                                             % Second Set Of Axes
+LinesAx2 = findobj(Ax2,'Type','Line');                      % Handle To Lines
+LinesAx2(2).LineWidth = Linewidth;                                  % Set ‘LineWidth’
+
+
 hold on
 bodemag(sys(fourth_patch(1),fourth_patch(2)))
+Fh = gcf;                                                   % Handle To Current Figure
+Kids = Fh.Children;                                         % Children
+AxAll = findobj(Kids,'Type','Axes');                        % Handles To Axes
+Ax1 = AxAll(1);                                             % First Set Of Axes
+LinesAx1 = findobj(Ax1,'Type','Line');                      % Handle To Lines
+LinesAx1(2).LineWidth = Linewidth;                                  % Set ‘LineWidth’
+Ax2 = AxAll(2);                                             % Second Set Of Axes
+LinesAx2 = findobj(Ax2,'Type','Line');                      % Handle To Lines
+LinesAx2(2).LineWidth = Linewidth;                                  % Set ‘LineWidth’
+
+
 hold on
 bodemag(sys(fifth_patch(1),fifth_patch(2)))
+Fh = gcf;                                                   % Handle To Current Figure
+Kids = Fh.Children;                                         % Children
+AxAll = findobj(Kids,'Type','Axes');                        % Handles To Axes
+Ax1 = AxAll(1);                                             % First Set Of Axes
+LinesAx1 = findobj(Ax1,'Type','Line');                      % Handle To Lines
+LinesAx1(2).LineWidth = Linewidth;                                  % Set ‘LineWidth’
+Ax2 = AxAll(2);                                             % Second Set Of Axes
+LinesAx2 = findobj(Ax2,'Type','Line');                      % Handle To Lines
+LinesAx2(2).LineWidth = Linewidth;                                  % Set ‘LineWidth’
 
-legend
+
+title('Bode diagram of the colocated pairs of patch' ,'FontSize', FontSize);
+
+xlabel( 'Frequency (Hz)', 'FontSize', FontSize);
+ylabel( 'Magnitude (dB)','FontSize', FontSize);
+
+[hleg1, hobj1] = legend('Pair 1','Pair 2','Pair 3','Pair 4','Pair 5');
+set(hleg1,'position',[0 0 0.5 0.5])
+textobj = findobj(hobj1, 'type', 'text');
+set(textobj, 'Interpreter', 'latex', 'fontsize', FontSize);
+
+
+ha = get(gca,'title');
+ha.String = 'From : Vai To : Vsi'; % remove the subtitle "From: u1 To: y1"
+
 
 %% Sisotool damp first mode on first patch
 
@@ -125,6 +194,21 @@ ylabel( 'Magnitude (dB)','FontSize', FontSize);
 set(hleg1,'position',[0 0 0.5 0.5])
 textobj = findobj(hobj1, 'type', 'text');
 set(textobj, 'Interpreter', 'latex', 'fontsize', FontSize);
+
+%% RL Siso
+controller = CLP1M1;
+BO=(controller)*sys(2,2);
+syssisocontrolled=feedback(sys,controller,first_patch(1),first_patch(2));
+P = pole(tf(syssisocontrolled(first_patch(1),first_patch(2))));
+rlocus(BO);
+grid on
+hold on
+plot(P,'x',Color='r');
+set(findall(gcf,'Type','line'),'LineWidth',Linewidth)
+title('Root Locus' ,'FontSize', FontSize);
+xlabel( 'Real axis', 'FontSize', FontSize);
+ylabel( 'Imagniray axis','FontSize', FontSize);
+
 %% Lazy MIMO controller
 load('CLP1M1.mat')
 
